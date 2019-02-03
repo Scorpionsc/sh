548430283c8c14373bef4c2e6d4ca48152386f97
Mykola Petrunin <petruninnikolay@gmail.com>
2019-02-03 09:12:33 +0200

    =start

--- source/screens/containers/ProductsScreenContainer.js
+++ source/screens/containers/ProductsScreenContainer.js
@@ -1,4 +1,4 @@
-import {connect} from 'react-redux';
+ import {connect} from 'react-redux';
 import ProductsScreen from "../components/ProductsScreen";
 import {removeProduct} from "../../store/products/actions";
 import {bindActionCreators} from "redux";
